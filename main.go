a5dfde835009630195e3b6b8b387a66149ada53d
Man Huynh Minh <hminhman@tma.com.vn>
2020-08-20 09:49:23 +0700

    Api post take json param. Add git ignore

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Login struct {
+	USER     string `json:"user" binding:"required"`
+	PASSWORD string `json:"password" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
-	r.POST("/form_post", func(c *gin.Context) {
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick", "anonymous")
 
-		c.JSON(200, gin.H{
-			"status":        "posted",
-			"messageGetted": message,
-			"nickGetted":    nick,
-		})
+	r.POST("/form_post", func(c *gin.Context) {
+		var loginReqPar Login
+		c.BindJSON(&loginReqPar)
+		c.JSON(200, gin.H{"status": "getted", "user": loginReqPar.USER})
 	})
 	r.Run()
 }
